3618d6c192d42d6f7734c78906322bcaded67c00
Anas Ghareib <AnasGhareib@gmail.com>
2024-05-10 04:52:39 +0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+import ImageViewer from './components/ImageViewer';
+import CustomButton from './components/CustomButton';
+
+const placeholderImage = require('./assets/images/background-image.png');
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.imageContainer}>
+        <ImageViewer placeholderImageSource={placeholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <CustomButton theme='primary' label='Choose a photo' />
+        {/* <Button theme='primary' label='Choose a photo' />
+        <Button label='Use this photo' /> */}
+      </View>
+      <StatusBar style='auto' />
     </View>
   );
 }
@@ -13,8 +26,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
 });
